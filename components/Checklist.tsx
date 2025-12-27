fbcec8df3b006b53c72f5f0fe6746192a4150414
fresche1 <fresche@fresche1.com>
2025-12-26 19:27:27 -0500

    fix: re-add ts-expect-error to bypass never error in toggleCleaning update

--- components/Checklist.tsx
+++ components/Checklist.tsx
@@ -132,6 +132,7 @@ const Checklist = ({ user }: { user: User }) => {
   const toggleCleaning = async (idx: number) => {
     const item = cleaning[idx];
     if (!item || !item.id) return;
+    // @ts-expect-error
     const { data, error } = await supabase!
       .from('checklist')
       .update({ complete: !item.complete })
