089746790da369b42adb464ad1f32a180b2f8b23
fresche1 <fresche@fresche1.com>
2025-12-26 19:04:39 -0500

    fix: type supabase client for User to resolve never error

--- utils/supabaseClient.ts
+++ utils/supabaseClient.ts
@@ -1,14 +1,19 @@
 import { createClient } from '@supabase/supabase-js';
+import type { User } from '../components/Dashboard';
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
 
-let supabase: ReturnType<typeof createClient> | null = null;
+type Database = {
+	users: User;
+};
+
+let supabase: ReturnType<typeof createClient<Database>> | null = null;
 if (!supabaseUrl || !supabaseAnonKey) {
 	console.error('Missing Supabase environment variables. Supabase client will not be initialized.');
 } else {
-	supabase = createClient(supabaseUrl, supabaseAnonKey);
+	supabase = createClient<Database>(supabaseUrl, supabaseAnonKey);
 }
 
 export { supabase };
