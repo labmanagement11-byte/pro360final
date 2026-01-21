af70238b1a9cba012e92e66133fbbb4b910f124f
fresche1 <fresche@fresche1.com>
2026-01-20 23:24:55 -0500

    Agregar Rutina de Mantenimiento como nueva sección en checklist

--- components/Dashboard.tsx
+++ components/Dashboard.tsx
@@ -135,6 +135,16 @@ const MANTENIMIENTO = {
     'Mantenimiento de palmeras: remover hojas secas.',
     'Mantener la matera de la terraza libre de maleza y deshierbar regularmente.',
     'Regar las plantas vivas según necesidad.'
+  ],
+  'RUTINA DE MANTENIMIENTO': [
+    'Mantener la piscina limpia y en funcionamiento.',
+    'Revisar constantemente el cuarto de máquinas para verificar su funcionamiento y detectar posibles filtraciones de agua.',
+    'Chequear que el generador eléctrico funcione correctamente y tenga diesel suficiente.',
+    'Encender la planta eléctrica al menos 2 veces al mes durante mínimo media hora.',
+    'Cortar el césped cada mes y medio a dos meses, y limpiar restos de césped.',
+    'Mantenimiento de palmeras: remover hojas secas.',
+    'Mantener la matera de la terraza libre de maleza y deshierbar regularmente.',
+    'Regar las plantas vivas según necesidad.'
   ]
 };
 
